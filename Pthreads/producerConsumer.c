b91720cbed0e4cdacb0948148aab7b21dfe1a49f
slichtenheld <slichtenheld@ufl.edu>
2017-01-30 21:48:25 -0500

    wrote code to generate producer consumer problem, not actually causing issues tho... Next is to use mutexes

--- Pthreads/producerConsumer.c
+++ Pthreads/producerConsumer.c
@@ -4,25 +4,50 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define BufferSize
+#define BufferSize 5
+
+/* global variables, is this the best way? */
+pthread_mutex_t mtx; // declare global mutex
+int counter = 0;
+#define COUNTMAX 20
 
 struct circularBuffer{
 	int in;
 	int out;
 	int numItems;
 	int buffer[BufferSize];
-};
+}; 
 
 void * producer(void * arg) {
-	
+	struct circularBuffer *circbuf = (struct circularBuffer*) arg;
+	for (; counter < COUNTMAX; counter++){
+		if ( circbuf->numItems < BufferSize ) {
+			circbuf->buffer[circbuf->in] = counter;
+			circbuf->in  = (circbuf->in + 1) % BufferSize;
+			circbuf->numItems++;
+			printf("adding %d to buffer, in: %d, numItems: %d\n", counter,circbuf->in,circbuf->numItems);
+		}
+		else counter--;
+	}
 }
 
 void * consumer(void * arg) {
-
+	struct circularBuffer *circbuf = (struct circularBuffer*) arg;
+	while(counter < COUNTMAX){
+		if (circbuf->numItems > 0){
+			printf("Removing %d from buffer\n",circbuf->buffer[circbuf->out] );
+			circbuf->out = (circbuf->out + 1) % BufferSize;
+			circbuf->numItems --;
+		}
+	}
 }
+
+
+/*****************************************************************/
 
 int main (int argc, char* argv[]) {
 
+	/*
 	if (argc != 2) {
 		perror("ERROR: ./program <size of buffer>");
 		exit(-1);
@@ -32,9 +57,10 @@ int main (int argc, char* argv[]) {
 	int bufferlength = strtol(argv[1],&endptr,10);
 
 	printf("Buffer is of size %d\n", bufferlength );
-
-	struct circularBuffer cBuf;
-	pthread_t prodThread,consThread;
+	*/
+	struct circularBuffer cBuf = {0,0,0,{0,0,0,0,0}}; /*make sure to initialize buffer*/
+	pthread_t prodThread,consThread; /* declare threads */
+	pthread_mutex_init(&mtx, NULL); /*create mutex*/
 
 	printf("Creating producer thread\n");
 	if ( pthread_create( &prodThread,NULL,producer,(void*)&cBuf ) ) { 
@@ -49,5 +75,7 @@ int main (int argc, char* argv[]) {
 		exit(-1);
 	}
 
+
 	pthread_exit(NULL); /* wait for all threads to terminate */
+	pthread_mutex_destroy(&mtx);
 }
