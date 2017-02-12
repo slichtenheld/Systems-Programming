5e722b8d7cdc95398661ceed46e47105eb7f571b
slichtenheld <slichtenheld@ufl.edu>
2017-02-11 19:01:09 -0500

    circBuf ADT works, needs full/empty checking

--- Deadlocks/assign3/circBuf.c
+++ Deadlocks/assign3/circBuf.c
@@ -1,26 +1,55 @@
-#include <fifo.h>
-
+#include "fifo.h"
+#include <stdlib.h>
+#include <assert.h>
 
 
 struct Fifo_t {
-	int size;
+	int size; // TODO: necessary?
 	int in;
 	int out;
 	int numItems;
-	void * items[];
-}
+	void * buffer[];
+};
 
 
 Fifo_T Fifo_new(int size) {
+	/* allocate memory */
 	Fifo_T fifo_rtn = malloc(sizeof(struct Fifo_t)+size*sizeof(void *));
-	fifo_rtn->size=size;
+	assert (fifo_rtn != NULL);
+	/* store_size */
+	fifo_rtn->size=size; // TODO: necessary?
+	fifo_rtn->in=0;
+	fifo_rtn->out=0;
+	fifo_rtn->numItems=0;
+	return fifo_rtn;
 }
 void Fifo_del(Fifo_T fifo){
+	assert(fifo);
 
+	free(fifo);
 }
 void Fifo_push(Fifo_T fifo, void *item){
+	assert(fifo);
 
+	fifo->buffer[fifo->in] = item;
+	fifo->in = (fifo->in + 1) % (fifo->size);
+	fifo->numItems++;
 }
 void* Fifo_pop(Fifo_T fifo){
+	assert(fifo);
 
+	void * temp = fifo->buffer[fifo->out];
+	fifo->out = (fifo->out + 1)% (fifo->size);
+	fifo->numItems--;
+	return temp;
+}
+extern int Fifo_size(Fifo_T fifo){
+	assert(fifo);
+
+	return fifo->size;
+}
+extern int Fifo_numItems(Fifo_T fifo){
+	assert(fifo);
+
+	return fifo->numItems;
 }
--- Deadlocks/assign3/fifo.h
+++ Deadlocks/assign3/fifo.h
@@ -2,7 +2,7 @@
 #define FIFO_H
 
 
-typedef struct Fifo *Fifo_T;
+typedef struct Fifo_t *Fifo_T;
 
 extern Fifo_T Fifo_new(int size);
 extern void Fifo_del(Fifo_T fifo);
@@ -10,5 +10,7 @@ extern void Fifo_del(Fifo_T fifo);
 extern void Fifo_push(Fifo_T fifo, void *item);
 extern void* Fifo_pop(Fifo_T fifo);
 
+extern int Fifo_size(Fifo_T fifo);
+extern int Fifo_numItems(Fifo_T fifo);
 
 #endif
